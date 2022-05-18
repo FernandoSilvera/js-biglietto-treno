842e16220dfde19c2863d10253badb16bc38a202
FernandoSilvera <fernando.silvera02@gmail.com>
2022-05-18 17:03:16 +0200

    Bonus completed.

---
+++ js/bonus.js
@@ -0,0 +1,62 @@
+/*
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+Formula per il calcolo: km * 0.21 = prezzoBiglietto
+
+Stampare in HTML una lista con eta inserità, km inseriti ed il prezzo finale
+eta: 50
+km: 120
+prezzzo: 24,50
+
+BONUS:
+validare/ controllare i dati inseriti
+età è un numero?
+età è stata inserita
+km è un numero valido?
+posso inserire un minimo / massimo di km possibili?
+*/
+
+let km = prompt("Quanti km vuoi percorrere?");
+let age = prompt("Quanti anni hai?");
+let docKm = document.getElementById("span-km");
+let docAge = document.getElementById("span-age");
+const docPrice = document.getElementById("span-price");
+
+if (isNaN(km)) {
+  km = null;
+  alert("I km inseriti non sono validi")
+} else if (km < 2) {
+  km = null;
+  alert("Il numero minimo di km è 2")
+  docKm.innerHTML = "errore: numero troppo piccolo";
+} else if (km > 300) {
+  km = null;
+  alert("Il numero massimo di km è 300")
+  docKm.innerHTML = "errore: numero troppo grande";
+}
+
+if (isNaN(age) || age <= 0) {
+  age = null;
+  alert("L'età inserita non è valida, non verranno applicati sconti.");
+  docAge.innerHTML = "sconosciuta";
+} else if (age <= 17) {
+  price = price - discountUnder18;
+  alert("Hai diritto a uno sconto del 20%");
+} else if (age >= 65) {
+  price = price - discountOver65;
+  alert("Hai diritto a uno sconto del 40%");
+}
+
+let price = km * 0.21;
+const discountUnder18 = price / 100 * 20;
+const discountOver65 = price / 100 * 40;
+
+
+docKm.innerHTML = km;
+docAge.innerHTML = age;
+docPrice.innerHTML = `${price.toFixed(2)}€`;
